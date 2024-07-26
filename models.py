b7b88fb05ac109ad7f27be134557e1cb40ef4dd0
paraigor <103633539+paraigor@users.noreply.github.com>
2024-07-26 18:16:13 +0300

    Обновил черновик моделей

--- models.py
+++ models.py
@@ -1,64 +1,97 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=200)
-    id_tg = models.CharField(max_length=200)
-    phone_number = models.IntegerField()
+    """Клиент"""
+
+    id_tg = models.UUIDField(
+        "id в телеграмм", blank=True, null=True, unique=True
+    )
+    full_name = models.CharField("ФИО", max_length=200)
+    phone_number = PhoneNumberField(null=True, unique=True, region="RU")
 
     def __str__(self) -> str:
-        return self.name
+        return self.full_name
 
 
 class Salon(models.Model):
-    title = models.CharField(max_length=200)
-    address = models.TextField()
+    """Салон"""
+
+    title = models.CharField("Название салона", max_length=100)
+    address = models.TextField("Адрес салона", max_length=200)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Service(models.Model):
+    """Услуга"""
+
+    title = models.CharField("Название услуги", max_length=200)
+    price = models.FloatField("Цена услуги")
+    duration = models.IntegerField("Длительность услуги, мин")
 
     def __str__(self) -> str:
         return self.title
 
 
 class Specialist(models.Model):
-    name = models.CharField(max_length=200)
+    """Специалист"""
+
+    full_name = models.CharField("ФИО", max_length=200)
 
     def __str__(self) -> str:
-        return self.name
+        return self.full_name
 
-
-class Service(models.Model):
-    title = models.CharField(max_length=200)
-    price = models.FloatField()
-
-    def __str__(self) -> str:
-        return self.title
-
-class TimeTable(models.Model):
-    salon = models.ForeignKey(Salon, null=True, on_delete=models.CASCADE)
-    specialict = models.ForeignKey(
-        Specialist, null=True, on_delete=models.CASCADE
-    )
-    service = models.ForeignKey(Service, null=True, on_delete=models.CASCADE)
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    occupied = models.BooleanField(default=False)
-
-
-    def __str__(self) -> str:
-        return f"{self.salon.title} {self.service.title}"
 
 class Order(models.Model):
-    STATUS = [("acc", "Принят"), ("cls", "Завершен"),]
-    PAYMENT = [
-        ("online", "На сайте"),
-        ("salon", "В салоне"),
-        ("paid", "Оплачено"),
-    ]
+    """Счёт (статус оплаты)"""
 
-    status = models.CharField(max_length=10, choices=STATUS)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    timetable = models.ForeignKey(TimeTable, on_delete=models.CASCADE)
-    payment = models.CharField(max_length=10, choices=PAYMENT)
+    STATUS = [
+        ("waiting", "Ожидает оплаты"),
+        ("paid", "Оплачено"),
+        ("cancel", "Отменено"),
+    ]
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, verbose_name="Клиент"
+    )
+    status = models.CharField("Статус заказа", max_length=14, choices=STATUS)
+    receipt = models.URLField("Чек", blank=True)
+    created_at = models.DateTimeField("Счёт выставлен", auto_now_add=True)
+    updated_at = models.DateTimeField("Последнее обновление", auto_now=True)
 
     def __str__(self) -> str:
-        return f"{self.client.name} {self.timetable.date}"
+        return f"{self.updated_at} {self.client.full_name} {self.status}"
+
+
+class SpecialistWorkDayInSalon(models.Model):
+    """Рабочий день сотрудника в одном из салонов"""
+
+    workday = models.DateField()
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE)
+    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE)
+    services = models.ManyToManyField(Service, verbose_name="Услуги")
+    start_at = models.TimeField()
+    end_at = models.TimeField()
+
+    def __str__(self) -> str:
+        return f"{self.specialist} {self.salon} {self.workday}"
+
+
+class Appointment(models.Model):
+    """Заказ услуги"""
+
+    STATUSES = [
+        ("access", "Принято"),
+        ("ended", "Завершено"),
+        ("discard", "Отменено"),
+    ]
+    status = models.CharField("Статус заказа", max_length=9, choices=STATUSES)
+    date = models.DateField()
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    start_at = models.TimeField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)

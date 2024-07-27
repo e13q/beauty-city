bb104b30e5ba989ae32ddffc343703d1f9bae532
e13q <110967581+e13q@users.noreply.github.com>
2024-07-27 18:25:49 +0300

    predeploy testing

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.app'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # Exit on error
 set -o errexit
 pip install -r requirements.txt
+pip install gunicorn
 
 # Apply any outstanding database migrations
 python backend/manage.py migrate

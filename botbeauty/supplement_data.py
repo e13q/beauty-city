a3662716fcba3fcc61e5638791a51bb620b9b026
Stmkv <stepkamakarov1999@mail.ru>
2024-07-27 14:45:04 +0300

    Обновил создание фейковых данных

--- botbeauty/supplement_data.py
+++ botbeauty/supplement_data.py
@@ -1,122 +1,98 @@
-import random
-import uuid
-from faker import Faker
+# import random
+# import uuid
+# from faker import Faker
 from mybot.models import Client, Salon, Service, Specialist, Order, SpecialistWorkDayInSalon, Appointment
 
 
-# ! pip install faker
-# ! запуск через PowerShell команда - >>> exec(open("supplement_data.py", encoding="utf-8").read())
+# # ! запуск через PowerShell команда - >>> exec(open("supplement_data.py", encoding="utf-8").read())
 
 
-fake = Faker('ru_RU')
+
+clients = [
+    {"id_tg": 224, "full_name": "Иван", "phone_number": "+79699998121"},
+    {"id_tg": 225, "full_name": "Мария", "phone_number": "+79686758122"},
+    {"id_tg": 226, "full_name": "Христина", "phone_number": "+79622228123"},
+    {"id_tg": 227, "full_name": "Кристина", "phone_number": "+79611118123"},
+    {"id_tg": 228, "full_name": "Егор", "phone_number": "+79676448123"},
+    {"id_tg": 229, "full_name": "Виталия", "phone_number": "+79612348123"},
+    {"id_tg": 230, "full_name": "Виталий", "phone_number": "+79601588123"},
+    {"id_tg": 231, "full_name": "Александр", "phone_number": "+79665968123"},
+    {"id_tg": 232, "full_name": "Дарья", "phone_number": "+79634567123"},
+    {"id_tg": 233, "full_name": "Светлана", "phone_number": "+79687998123"},
+    {"id_tg": 234, "full_name": "Максим", "phone_number": "+79631238123"},
+    {"id_tg": 235, "full_name": "Анна", "phone_number": "+79665828123"}
+]
+
+client_objects = [Client.objects.create(**client) for client in clients]
 
 
-def generate_phone_number():
-    return f"+7{fake.msisdn()[1:]}"
+salons = [
+    {"title": "Tip-Top", "address": "г. Москва ул. Красноармейская, д. 1"},
+    {"title": "Glamour", "address": "г. Москва ул. Ленина, д. 5"},
+    {"title": "Beauty Plus", "address": "г. Москва ул. Тверская, д. 10"}
+]
+
+salon_objects = [Salon.objects.create(**salon) for salon in salons]
 
 
-def generate_url():
-    return f"https://example.com/receipt/{uuid.uuid4()}"
+services = [
+    {"title": "Окрашивание", "price": 4500.0, "duration": 180},
+    {"title": "Маникюр", "price": 1500.0, "duration": 60},
+    {"title": "Стрижка", "price": 2000.0, "duration": 90},
+    {"title": "Педикюр", "price": 2500.0, "duration": 90}
+]
 
-# TODO Нужо протестировать. (Обновленная функция, чтобы даты были с 2020 по 2025)
-# def generate_date():
-#     start_date = datetime(2020, 1, 1)
-#     end_date = datetime(2024, 12, 31)
-#     delta = end_date - start_date
-#     random_days = random.randint(0, delta.days)
-#     random_date = start_date + timedelta(days=random_days)
-#     return random_date.strftime("%Y-%m-%d")
+service_objects = [Service.objects.create(**service) for service in services]
 
 
-def generate_date():
-    return f"2024-08-{random.randint(1, 31):02d}"
+specialists = [
+    {"full_name": "Иван Иван Иваныч"},
+    {"full_name": "Петр Петрович"},
+    {"full_name": "Мария Ивановна"},
+    {"full_name": "Александр Ракшин"},
+    {"full_name": "Ксения Кузнецова"},
+    {"full_name": "Мария Петровна"},
+    {"full_name": "Сергей Сергеевич"}
+]
+
+specialist_objects = [Specialist.objects.create(**specialist) for specialist in specialists]
 
 
-# Создание клиентов
-clients = []
-for _ in range(100):
-    clients.append(Client.objects.create(
-        id_tg=random.randint(1, 10000),
-        full_name=fake.name(),
-        phone_number=generate_phone_number()
-    ))
+orders = [
+    {"client": client_objects[0], "status": 'paid', "receipt": 'https://www.google.com'},
+    {"client": client_objects[1], "status": 'waiting', "receipt": 'https://www.yandex.ru'},
+    {"client": client_objects[2], "status": 'cancel', "receipt": 'https://www.mail.ru'},
+    {"client": client_objects[3], "status": 'paid', "receipt": 'https://www.example.com'},
+    {"client": client_objects[4], "status": 'waiting', "receipt": 'https://www.example.org'},
+    {"client": client_objects[5], "status": 'cancel', "receipt": 'https://www.example.net'}
+]
+
+order_objects = [Order.objects.create(**order) for order in orders]
 
 
-# Создание салонов
-salons = []
-for _ in range(10):
-    salons.append(Salon.objects.create(
-        title=fake.company(),
-        address=fake.address()
-    ))
-
-# Создание услуг
-services = []
-services_list = [("Окрашивание", 4500.0, 180),
-                 ("Маникюр", 1500.0, 60),
-                 ("Стрижка", 2000.0, 90),
-                 ("Педикюр", 2500.0, 90)]
-for title, price, duration in services_list:
-    for _ in range(10):
-        services.append(Service.objects.create(
-            title=f"{title} {fake.word()}",
-            price=price,
-            duration=duration
-        ))
-
-# Создание специалистов
-specialists = []
-for _ in range(20):
-    specialists.append(Specialist.objects.create(
-        full_name=fake.name()
-    ))
-
-# Создание заказов
-orders = []
-for _ in range(300):
-    client = random.choice(clients)
-    orders.append(Order.objects.create(
-        client=client,
-        status=random.choice(['paid', 'waiting', 'cancel']),
-        receipt=generate_url()
-    ))
-
-# Создание рабочих дней специалистов в салонах
 workdays = []
-for _ in range(200):
-    salon = random.choice(salons)
-    specialist = random.choice(specialists)
-    workday = generate_date()
-    start_at = f"{random.randint(8, 10)}:00"
-    end_at = f"{random.randint(17, 19)}:00"
-    workday_obj = SpecialistWorkDayInSalon.objects.create(
-        workday=workday,
-        salon=salon,
-        specialist=specialist,
-        start_at=start_at,
-        end_at=end_at
-    )
-    workday_obj.services.set(random.sample(services, k=random.randint(1, 4)))
-    workdays.append(workday_obj)
+for i in range(10):
+    for j, specialist in enumerate(specialist_objects):
+        workday = {"workday": f'2024-08-{i+1:02d}', "salon": salon_objects[(i + j) % len(salon_objects)], "specialist": specialist, "start_at": '09:00', "end_at": '18:00'}
+        workday_obj = SpecialistWorkDayInSalon.objects.create(**workday)
 
-# Создание записей о заказах услуг
+        if (i + j) % 2 == 0:
+            workday_obj.services.add(service_objects[0], service_objects[1])
+        else:
+            workday_obj.services.add(service_objects[2], service_objects[3])
+        workdays.append(workday_obj)
+
+
 appointments = []
-for _ in range(500):
-    status = random.choice(['access', 'ended', 'discard'])
-    date = generate_date()
-    salon = random.choice(salons)
-    client = random.choice(clients)
-    specialist = random.choice(specialists)
-    service = random.choice(services)
-    start_at = f"{random.randint(8, 16)}:00"
-    order = random.choice(orders)
-    appointments.append(Appointment.objects.create(
-        status=status,
-        date=date,
-        salon=salon,
-        client=client,
-        specialist=specialist,
-        service=service,
-        start_at=start_at,
-        order=order
-    ))
+for i, workday in enumerate(workdays):
+    appointments.extend([
+        {"status": 'access', "date": workday.workday, "salon": workday.salon, "client": client_objects[i % len(client_objects)], "specialist": workday.specialist, "service": service_objects[0], "start_at": '09:00', "order": order_objects[i % len(order_objects)]},
+        {"status": 'ended', "date": workday.workday, "salon": workday.salon, "client": client_objects[(i + 1) % len(client_objects)], "specialist": workday.specialist, "service": service_objects[1], "start_at": '11:00', "order": order_objects[(i + 1) % len(order_objects)]},
+        {"status": 'discard', "date": workday.workday, "salon": workday.salon, "client": client_objects[(i + 2) % len(client_objects)], "specialist": workday.specialist, "service": service_objects[2], "start_at": '13:00', "order": order_objects[(i + 2) % len(order_objects)]},
+        {"status": 'access', "date": workday.workday, "salon": workday.salon, "client": client_objects[(i + 3) % len(client_objects)], "specialist": workday.specialist, "service": service_objects[3], "start_at": '15:00', "order": order_objects[(i + 3) % len(order_objects)]},
+        {"status": 'ended', "date": workday.workday, "salon": workday.salon, "client": client_objects[(i + 4) % len(client_objects)], "specialist": workday.specialist, "service": service_objects[0], "start_at": '17:00', "order": order_objects[(i + 4) % len(order_objects)]},
+        {"status": 'discard', "date": workday.workday, "salon": workday.salon, "client": client_objects[(i + 5) % len(client_objects)], "specialist": workday.specialist, "service": service_objects[1], "start_at": '18:00', "order": order_objects[(i + 5) % len(order_objects)]}
+    ])
+
+for appointment in appointments:
+    Appointment.objects.create(**appointment)

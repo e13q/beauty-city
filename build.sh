c9b900c7b226ee786c457127499d643324be69d0
e13q <110967581+e13q@users.noreply.github.com>
2024-07-28 17:13:36 +0300

    Clean repo

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-pip install -r requirements.txt
-pip install gunicorn
-pip install uvicorn
-
-# Apply any outstanding database migrations
-python backend/manage.py migrate
